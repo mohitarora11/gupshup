0d92d686fcc87b2c1f721fb0b9fafe7a50563115
pooja singh <poojakumari1389@gmail.com>
2014-07-24 10:59:14 +0530

    images of special offer page

--- specialoffer.php
+++ specialoffer.php
@@ -17,13 +17,13 @@ The Card of your choice at Cyber Hub<br/><span style="font-size:12px">Exclusive 
 <ul>
 <li><span><img src="images/dunkindonuts.jpg" width="60" ></span><p>20% discount at Pizza Hut</p></li>
 <li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount at Dunkin Donuts</p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount at Zambar</p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount at The Beer Cafe </p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>15% discount at Rred Hot </p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>5% discount</p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount on Italiano</p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount on Krispy Kreme</p></li>
-<li><span><img src="images/pizzahut.jpg" width="60" ></span><p>10% discount on au bon pain</p></li>
+<li><span><img src="images/zombar.jpg" width="60" ></span><p>10% discount at Zambar</p></li>
+<li><span><img src="images/beercafe.jpg" width="60" ></span><p>10% discount at The Beer Cafe </p></li>
+<li><span><img src="images/redhot.jpg" width="60" ></span><p>15% discount at Rred Hot </p></li>
+<li><span><img src="images/panchvati.jpg" width="60" ></span><p>5% discount at Panchvati Gaurav</p></li>
+<li><span><img src="images/italiano.jpg" width="60" ></span><p>10% discount on Italiano</p></li>
+<li><span><img src="images/krispykreme.jpg" width="60" ></span><p>10% discount on Krispy Kreme</p></li>
+<li><span><img src="images/aubonpain.jpg" width="60" ></span><p>10% discount on au bon pain</p></li>
 <li><span><img src="images/rsz_thumb-13.jpg" width="60" ></span><p>15% discount on cafe Delhi Heights</p></li>
 <li><span><img src="images/rsz_thumb-14.jpg" width="60" ></span><p>10% discount on Joost Juice bars</p></li>
 <li><span><img src="images/rsz_thumb-15.jpg" width="60" ></span><p>15% discount on Raasta</p></li>
