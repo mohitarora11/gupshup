d2063e74ab65a578b068ac89edb487fcfc49a18f
mohit <mohit.11.arora@gmail.com>
2014-08-03 16:16:42 +0530

    changes

--- leaderboard.php
+++ leaderboard.php
@@ -1,8 +1,88 @@
 <?php
 include_once('globalvar.php');
 include_once('app_config.php');
-include_once('sql.php');
 
+
+require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
+require_once( 'Facebook/HttpClients/FacebookCurl.php' );
+require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
+ 
+require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookCanvasLoginHelper.php' );
+require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'Facebook/FacebookRequest.php' );
+require_once( 'Facebook/FacebookResponse.php' );
+require_once( 'Facebook/FacebookSDKException.php' );
+require_once( 'Facebook/FacebookRequestException.php' );
+require_once( 'Facebook/FacebookAuthorizationException.php' );
+require_once( 'Facebook/GraphObject.php' );
+
+// path of these files have changes
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+ 
+use Facebook\FacebookSession;
+use Facebook\FacebookCanvasLoginHelper;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+
+// init app with app id (APPID) and secret (SECRET)
+FacebookSession::setDefaultApplication(APPID, APPSECRET);
+
+
+if(isset($_REQUEST['error']) ){
+
+	$helper = new FacebookRedirectLoginHelper(CANVASURL);
+	$LOGINURL = $helper->getLoginUrl(explode(',',SCOPE));
+	//include_once('home.php');
+	header("Location: ".$GLOBALS['url']."index.php"); /* Redirect browser */
+	die('');
+}
+
+//for canvas
+$helper = new FacebookCanvasLoginHelper();
+try {
+  $session = $helper->getSession();
+  //print_r($session); 
+} catch (FacebookRequestException $ex) {
+	// When Facebook returns an error
+	//print_r($ex);
+} catch (\Exception $ex) {   
+	// When validation fails or other local issues
+	//print_r($ex);
+}
+if ($session){
+	$request = new FacebookRequest( $session, 'GET', '/me' );
+	$response = $request->execute();  
+	$graphObject = $response->getGraphObject();
+	$_SESSION["userid"]=$graphObject->getProperty('id');
+	$_SESSION["useremail"]=$graphObject->getProperty('email');
+	$_SESSION["username"]=$graphObject->getProperty('name');
+	$_SESSION["facebook_session"] = $session;	
+	//$page = $GLOBALS['url']."savedata.php?PHPSESSID=".session_id();
+	//print_r($_SESSION);
+	//header("Location: ".$page);
+	//exit();
+}else{
+
+	$helper = new FacebookRedirectLoginHelper(CANVASURL);
+	$LOGINURL = $helper->getLoginUrl(explode(',',SCOPE));
+	$_SESSION["LOGINURL"]= $LOGINURL;
+	
+?>
+	<script>
+		top.window.location.href = '<?php echo $helper->getLoginUrl(explode(',',SCOPE));?>';
+	</script>
+<?php
+}
+
+include_once('sql.php');
 include_once ('header.php');
 //$_SESSION["userid"] = '111';
 if($_SESSION["userid"]){
