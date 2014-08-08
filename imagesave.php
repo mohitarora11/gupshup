173fa52d2f9ffc93cd1687564f8599b1ce7c4e38
pooja singh <poojakumari1389@gmail.com>
2014-08-08 09:36:09 +0530

    changes

--- imagesave.php
+++ imagesave.php
@@ -8,8 +8,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $image = new SimpleImage();
     $image->load($_FILES["file"]["tmp_name"]);
     if ($image->getWidth() > 500 && $image->getHeight() > 500) {
-		$s= explode("\.", $_FILES["file"]["name"]);
-        $resizedphotourl = 'resize_' . time() . $s[count($s)-1];
+		$s= explode(".", $_FILES["file"]["name"]);
+        $resizedphotourl = 'resize_' . time() .'.'. $s[count($s)-1];
         $image->resize(500, 500);
         $i = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'resizedimages' . DIRECTORY_SEPARATOR . $resizedphotourl;
         $image->save($i);
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         imagecopymerge($dest, $srcimg->image, 21, 21, 0, 0, $srcimg->getWidth(), $srcimg->getHeight(), 100);
         // Output and free from memory
         //header('Content-Type: image/jpeg');
-        $framephotourl = 'frame_' . time() . $s[count($s)-1];
+        $framephotourl = 'frame_' . time() .'.'. $s[count($s)-1];
         imagejpeg($dest, dirname(__FILE__) . DIRECTORY_SEPARATOR . 'resizedimages' . DIRECTORY_SEPARATOR . $framephotourl);
         saveoption($_SESSION["userid"], $_POST["optionchosen"], $framephotourl, $resizedphotourl);
         savestatus($_SESSION["userid"], 1);
